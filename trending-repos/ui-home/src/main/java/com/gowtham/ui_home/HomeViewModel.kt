42e4e106789341f0d360861f710294c70c372961
gowtham <a914gowtham@gmail.com>
2022-01-16 00:44:45 +0530

    :star: feature: swipe to refresh added

--- trending-repos/ui-home/src/main/java/com/gowtham/ui_home/HomeViewModel.kt
+++ trending-repos/ui-home/src/main/java/com/gowtham/ui_home/HomeViewModel.kt
@@ -13,46 +13,46 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-     private val useCases: RepoUseCases
-) : ViewModel(){
+    private val useCases: RepoUseCases
+) : ViewModel() {
 
-     private val _trendingRepoList =
-          MutableStateFlow<ResultState<List<Repository>>>(ResultState.Idle)
-     val listState: StateFlow<ResultState<List<Repository>>>
-          get() = _trendingRepoList
+    private val _trendingRepoList =
+        MutableStateFlow<ResultState<List<Repository>>>(ResultState.Idle)
+    val listState: StateFlow<ResultState<List<Repository>>>
+        get() = _trendingRepoList
 
-     private val _refreshState =
-          MutableStateFlow(false)
-     val refreshState: StateFlow<Boolean>
-          get() = _refreshState
+    private val _refreshState =
+        MutableStateFlow(false)
+    val refreshState: StateFlow<Boolean>
+        get() = _refreshState
 
-     init {
-         fetchRepoList()
-     }
+    init {
+        fetchRepoList()
+    }
 
-     fun fetchRepoList() {
-          if (_trendingRepoList.value is ResultState.Loading)
-               return
-          _trendingRepoList.value = ResultState.Loading
-          viewModelScope.launch {
-               _trendingRepoList.value = useCases.getRepos.execute(refresh = false)
-          }
-     }
+    fun fetchRepoList() {
+        if (_trendingRepoList.value is ResultState.Loading)
+            return
+        _trendingRepoList.value = ResultState.Loading
+        viewModelScope.launch {
+            _trendingRepoList.value = useCases.getRepos.execute(refresh = false)
+        }
+    }
 
-     fun refreshRepoList() {
-          if (_refreshState.value)
-               return
-          _refreshState.value = true
-          viewModelScope.launch {
-               _trendingRepoList.value = useCases.getRepos.execute(refresh = true)
-               _refreshState.value= false
-          }
-     }
+    fun refreshRepoList() {
+        if (_refreshState.value)
+            return
+        _refreshState.value = true
+        viewModelScope.launch {
+            _trendingRepoList.value = useCases.getRepos.execute(refresh = true)
+            _refreshState.value = false
+        }
+    }
 
-     fun queryRepo(query: String) {
-          viewModelScope.launch {
-               _trendingRepoList.value = useCases.searchRepoUseCase.execute(query)
-          }
-     }
+    fun queryRepo(query: String) {
+        viewModelScope.launch {
+            _trendingRepoList.value = useCases.searchRepoUseCase.execute(query)
+        }
+    }
 
 }
